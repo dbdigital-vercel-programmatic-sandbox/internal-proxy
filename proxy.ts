951a7e453685c5e136ebae5875d5729c688862d6
Bhaskar Builder <builder@bhaskar.dev>
2026-03-26 18:03:16 +0000

    initial commit

---
+++ proxy.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+    const host = req.headers.get("host") || "";
+    const subdomain = host.replace(".p.bhaskarapp.com", "");
+    
+    if (!subdomain || subdomain === host) {
+        // Someone hit p.bhaskarapp.com directly
+        return new NextResponse("Not found", { status: 404 });
+    }
+    
+    const target = new URL(
+        `${req.nextUrl.pathname}${req.nextUrl.search}`,
+        // <data>.p.bhaskarapp.com -> <data>-db-digital.vercel.app
+        `https://${subdomain}-db-digital.vercel.app`
+    );
+
+    return NextResponse.rewrite(target);
+}
+
+export const config = {
+    matcher: "/(.*)",
+};
